6102022825554ff0ff330c0ef818e6e43f2e82dc
Winton <ios@churinc.com>
2020-07-12 22:25:44 +1000

    Initial Commit

---
+++ AttendanceSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttendanceSystem
+//
+//  Created by LIWEIJIE on 12/7/20.
+//  Copyright © 2020 weijie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
